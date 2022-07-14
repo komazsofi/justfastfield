62da3c4ab2e05f6a2bfb79089e1be347a882a28b
Zsofia Koma <komazsofi@gmail.com>
2022-07-14 09:06:16 +0200

    basemap prep + novana prep

---
+++ extract_classes_frombasemap.R
@@ -0,0 +1,42 @@
+library(terra)
+
+# Set working directory
+workingdirectory="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/forHanne_basemap_extract/"
+setwd(workingdirectory)
+
+basemapfile="lu_00_2018.tif"
+
+# Import
+
+basemap=rast(basemapfile)
+
+# Automatically extract the §3 and natura-2000 habitats (a 0-1 mask)
+
+listofcodes=c(30000100,30000200,30000300,30000400,30000500,30000600,
+              40121000,40122000,40123000,40131000,40132000,40133000,40134000,
+              40211000,40212000,40213000,40214000,40216000,40217000,40218000,40219000,
+              40225000,40231000,40232000,40233000,40401000,40403000,40513000,40612000,
+              40621000,40623000,40641000,40643000,40711000,40712000,40714000,40715000,
+              40721000,40722000,40723000,40822000,40910100,40910200,40911000,40912000,
+              40913000,40915000,40916000,40917000,40919000)
+
+for (i in listofcodes) {
+  print(i)
+  
+  basemap_layer <- clamp(basemap, i, i,values=FALSE) 
+  writeRaster(basemap_layer,paste("basemap_class_",i,".tif",paste=""),overwrite=TRUE)
+  
+}
+
+# only ph3
+
+basemap_layer_ph3 <- clamp(basemap, 30000100, 30000600,values=FALSE) 
+writeRaster(basemap_layer_ph3,paste("basemap_class_ph3all.tif",paste=""),overwrite=TRUE)
+
+# only natura-2000
+
+basemap_layer_nat2000 <- clamp(basemap, 40121000, 40919000,values=FALSE) 
+writeRaster(basemap_layer_nat2000,paste("basemap_class_nat2000all.tif",paste=""),overwrite=TRUE)
+
+
+
---
+++ prep_novanaplots.R
@@ -0,0 +1,55 @@
+library(data.table)
+library(sf)
+library(raster)
+
+library(tidyverse)
+
+CreateShape = function(data) {
+  
+  library(sp)
+  
+  data$X_obs=data$UTM_X_orig
+  data$Y_obs=data$UTM_Y_orig
+  
+  shp=data
+  coordinates(shp)=~X_obs+Y_obs
+  proj4string(shp)<- CRS("+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+  
+  return(shp)
+  
+}
+
+# Import
+
+data<-as.data.frame(fread("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/_PostDoc_Paper1_plantdivfromlidar/Dataset/fielddata/novana/NOVANAAndP3_tozsofia/NOVANAAndP3_tozsofia.tsv",
+                          encoding="UTF-8"))
+
+ph3_eng=stack("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/forHanne_basemap_extract/basemap_class_ 30000100 .tif")
+
+# Create plot data shapefile
+
+data_5m=data[is.na(data$Plot5mID)==FALSE,]
+data_15m=data[is.na(data$Plot5mID)==TRUE,]
+
+data_plot_5m=data_5m %>% 
+  group_by(Plot5mID,Yeare,Habitat,HabitatID,UTM_X_orig,UTM_Y_orig) %>%
+  summarize(SpRichness=n())
+
+data_plot_15m=data_15m %>% 
+  group_by(Plot15mID,Yeare,Habitat,HabitatID,UTM_X_orig,UTM_Y_orig) %>%
+  summarize(SpRichness=n())
+
+data_plot_5m_shp=CreateShape(data_plot_5m)
+
+# Only within eng habitats
+
+eng <- raster::extract(ph3_eng, data_plot_5m_shp) 
+data_plot_5m_shp$eng=eng[,1]
+
+data_plot_5m_witheng=data_plot_5m_shp@data 
+data_plot_5m_witheng_filt=na.omit(data_plot_5m_witheng)
+
+data_plot_5m_witheng_filt_shp=CreateShape(data_plot_5m_witheng_filt)
+
+shapefile(data_plot_5m_witheng_filt_shp,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/data_plot_5m_onlyeng.shp")
+
