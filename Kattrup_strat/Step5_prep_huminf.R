8d172bd7c0400280cb4c9ba7a4e34e61750feb0e
komazsofi <komazsofi@gmail.com>
2022-07-19 17:00:13 +0200

    human infrastr

---
+++ Kattrup_strat/Step5_prep_huminf.R
@@ -0,0 +1,15 @@
+library(rgdal)
+library(raster)
+library(tidyverse)
+library(rgeos)
+
+# import
+
+study_area=readOGR(dsn="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_datasets/Kattrup_Vildnis_extent_w500mbuffer.shp")
+asreference=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/lidar_vegetation_density.tif")
+
+roads=readOGR(dsn="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_datasets/human_infrastructure/VEJMIDTE.shp")
+
+# crop to area of interest
+
+roads_crop=crop(roads,study_area)
