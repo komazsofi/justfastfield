780f4e0feea6e978b9b1fdaf8382de2b2628014d
komazsofi <komazsofi@gmail.com>
2022-07-19 18:56:13 +0200

    distance from road layer

--- Kattrup_strat/Step5_prep_huminf.R
+++ Kattrup_strat/Step5_prep_huminf.R
@@ -13,3 +13,19 @@ roads=readOGR(dsn="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_field
 # crop to area of interest
 
 roads_crop=crop(roads,study_area)
+#shapefile(roads_crop,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_datasets/human_infrastructure/VEJMIDTE_crop.shp")
+
+# create a raster which measures the distance from the route
+
+distroad_r <- raster()
+
+extent(distroad_r) <- extent(asreference)
+res(distroad_r) <- res(asreference) 
+
+distroad_r_wvalue = gDistance(roads_crop, as(distroad_r,"SpatialPoints"), byid=TRUE)
+
+distroad_r[] = apply(distroad_r_wvalue,1,min)
+crs(distroad_r)=crs(asreference)
+
+writeRaster(distroad_r,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/human_distfromroad.tif")
+
