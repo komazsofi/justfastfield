77dc264b82ff4d66014b46c6b54805b86e0cff84
komazsofi <komazsofi@gmail.com>
2022-08-05 15:30:42 +0200

    add forest assemleges

--- Kattrup_strat/Step4b_prep_forest.R
+++ Kattrup_strat/Step4b_prep_forest.R
@@ -11,16 +11,21 @@ forestmap=readOGR(dsn="O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_f
 
 # Filter only important tree species based on irina excel file
 
-forestmap_int=forestmap[forestmap@data$ART %in% c("ALÃ˜","ask","bir","BÃ˜G","dgr","eg","el","KRT","LÃ†R","ngr","ngrj",
+forestmap_int=forestmap[forestmap@data$ART %in% c("ALÃ˜","ask","bir","BÃ˜G","dgr","eg","el","LÃ†R","ngr","ngrj",
                                               "ngrp","nob","nobp","pil","pop","rel","rem","rgr","sgr","skf"
                                               ,"thu","VÃ†R","Ã†GR","Ã†R","v�r"),]
-
-shapefile(forestmap_int,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_datasets/fromIrina/GIS/Skovkort/kattrup_skovkort_polygon_selected.shp",overwrite=TRUE)
 
 forestmap_int_sel=forestmap_int[(forestmap_int@data$AARGANG!=9999 & forestmap_int@data$AARGANG!=0),]
 plot(forestmap_int_sel$ALDER,forestmap_int_sel$AARGANG,col=factor(forestmap_int_sel$ART),pch=16,xlab="ALDER",ylab="AARGANG")
 
 # group into assemblages
+
+forestmap_int_gr=forestmap_int
+forestmap_int_gr[forestmap_int_gr@data$ART=="BÃ˜G",]<-1
+forestmap_int_gr[forestmap_int_gr@data$ART=="eg",]<-2
+forestmap_int_gr[forestmap_int_gr@data$ART=="el",]<-3
+forestmap_int_gr[forestmap_int_gr@data$ART %in% c("ALÃ˜","ask","bir","pil","pop","rel","rem","VÃ†R","v�r"),]<-4
+forestmap_int_gr[forestmap_int_gr@data$ART %in% c("dgr","LÃ†R","ngr","ngrj","ngrp","nob","nobp","rgr","sgr","skf","thu","Ã†GR","Ã†R")]<-5
 
 # rasterize info
 
@@ -38,7 +43,13 @@ crs(loc_age_r_c)=crs(asreference)
 loc_establish_r_c <- rasterize(forestmap_int_sel, loc_species_r,"AARGANG")
 crs(loc_establish_r_c)=crs(asreference)
 
+forestmap_int_gr$sp_fact=as.factor(forestmap_int_gr$ART)
+forestmap_int_gr_c <- rasterize(forestmap_int_gr, loc_species_r,"sp_fact")
+crs(forestmap_int_gr_c)=crs(asreference)
+
 # export
 writeRaster(loc_species_r_c,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/forest_species_kattrup.tif",overwrite=TRUE)
 writeRaster(loc_age_r_c,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/forest_age_kattrup.tif",overwrite=TRUE)
 writeRaster(loc_establish_r_c,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/forest_continiuty_kattrup.tif",overwrite=TRUE)
+
+writeRaster(forestmap_int_gr_c,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/forest_assembly_kattrup.tif",overwrite=TRUE)
