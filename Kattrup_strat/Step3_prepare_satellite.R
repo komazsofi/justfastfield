fa1aad0352e2f7c341c123aea399e09bc2106ac5
komazsofi <komazsofi@gmail.com>
2022-08-05 14:36:21 +0200

    add min ndvi

--- Kattrup_strat/Step3_prepare_satellite.R
+++ Kattrup_strat/Step3_prepare_satellite.R
@@ -11,6 +11,7 @@ asreference=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fiel
 
 S2_NDVI_p90=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_satellite_datasets/S2_NDVI_p90_utm_2021.tif")
 S2_NDWI_p90=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_satellite_datasets/S2_NDWI_p90_utm_2021.tif")
+S2_NDVI_p10=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_satellite_datasets/S2_NDVI_p10_utm_2021.tif")
 
 L_NDVI_std_30y=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_satellite_datasets/NDVIstd_timeseries_std_utm_1990_2020.tif")
 L_NDVI_med_30y=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Input_satellite_datasets/NDVIstd_timeseries_med_utm_1990_2020.tif")
@@ -19,6 +20,7 @@ L_NDVI_med_30y=raster("O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_f
 
 S2_NDVI_p90_resampl=resample(S2_NDVI_p90,asreference)
 S2_NDWI_p90_resampl=resample(S2_NDWI_p90,asreference)
+S2_NDVI_p10_resampl=resample(S2_NDVI_p10,asreference)
 
 L_NDVI_std_30y_resampl=resample(L_NDVI_std_30y,asreference)
 L_NDVI_med_30y_resampl=resample(L_NDVI_med_30y,asreference)
@@ -27,6 +29,7 @@ L_NDVI_med_30y_resampl=resample(L_NDVI_med_30y,asreference)
 
 writeRaster(S2_NDVI_p90_resampl,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/satelite_S2_NDVI_p90.tif")
 writeRaster(S2_NDWI_p90_resampl,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/satelite_S2_NDWI_p90.tif")
+writeRaster(S2_NDVI_p10_resampl,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/satelite_S2_NDVI_p10.tif")
 
 writeRaster(L_NDVI_std_30y_resampl,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/satelite_L_NDVI_std_30y.tif")
 writeRaster(L_NDVI_med_30y_resampl,"O:/Nat_Sustain-proj/_user/ZsofiaKoma_au700510/Justquick_fielddata/_Kattrup_Stratification/Organized_raster_layers/satelite_L_NDVI_med_30y.tif")
